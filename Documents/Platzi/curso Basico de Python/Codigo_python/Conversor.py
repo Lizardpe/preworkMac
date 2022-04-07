a5c7dc0ea115eef716c6cd7e588abd2e35c7c6d1
Roger <rgr_cg@hotmail.com>
2022-04-07 02:45:07 -0500

    conversor de moneda

---
+++ Documents/Platzi/curso Basico de Python/Codigo_python/Conversor.py
@@ -0,0 +1,30 @@
+def conversor(tipo_pesos,valor_dolar):
+    pesos = input("¿Cuánto pesos "+tipo_pesos+" tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares) #convierte el resultado numerico en un string
+    print("Tienes $" + dolares + " dólares")
+
+
+
+menu = """
+Bienvenido al conversor de monedas 💰🤖💰
+
+1 - Pesos colombianos 🇨🇴
+2 - Pesos argentinos 🇦🇷
+3 - Pesos mexicanos 🇲🇽
+
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("colombianos", 3875)
+elif opcion == 2:
+   conversor("argentinos",65)
+elif opcion == 3:
+    conversor("mexicanos",24)
+else:
+    print("Ingresa una opción correcta por favor")
